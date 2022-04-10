89e6a4e497d8523d45c939c8d56edfc6cd3cd410
Ben Clark <benclarkphoto@mac.com>
2022-04-10 14:43:00 -0700

    add js to generate random colors

---
+++ app.js
@@ -0,0 +1,32 @@
+// Global selections and variables
+const colorDivs = document.querySelectorAll('.color');
+const generateBtn = document.querySelector('.generate');
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll('.color h2');
+let intialColors;
+
+// Functions
+
+// Color Generator
+function generateHex() {
+    const letters = '#0123456789ABCDEF';
+    let hash = '#';
+
+    for(let i = 0; i < 6; i++) {
+        hash += letters[Math.floor(Math.random() * 16)];
+    } 
+    return hash;
+}
+
+function randomColors() {
+    colorDivs.forEach((div, index) => {
+        const hexText = div.children[0];
+        const randomColor = generateHex();
+
+        //add color to background
+        div.style.backgroundColor = randomColor;
+        hexText.innerText = randomColor;
+    });
+}
+
+randomColors();
