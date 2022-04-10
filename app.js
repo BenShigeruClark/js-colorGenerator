638f6952fe8aade26852dc976d996b56add333f5
Ben Clark <benclarkphoto@mac.com>
2022-04-10 15:19:16 -0700

    add text color change functionality

--- app.js
+++ app.js
@@ -9,13 +9,8 @@ let intialColors;
 
 // Color Generator
 function generateHex() {
-    const letters = '#0123456789ABCDEF';
-    let hash = '#';
-
-    for(let i = 0; i < 6; i++) {
-        hash += letters[Math.floor(Math.random() * 16)];
-    } 
-    return hash;
+    const hexColor = chroma.random();
+    return hexColor;
 }
 
 function randomColors() {
@@ -26,7 +21,18 @@ function randomColors() {
         //add color to background
         div.style.backgroundColor = randomColor;
         hexText.innerText = randomColor;
+        // check for contrast
+        checkTextContrast(randomColor, hexText);
     });
+}
+
+function checkTextContrast(color, text) {
+    const luminance = chroma(color).luminance();
+    if(luminance > 0.5) {
+        text.style.color = "black"; 
+    } else {
+        text.style.color = "white";
+    }
 }
 
 randomColors();
